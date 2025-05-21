66066fdd1a6329a04e7ace6c0c1cacb7e4469103
Tomáš Znamenáček <zoul@zoul.cz>
2025-05-21 18:02:26 +0200

    Improve score distribution chart

--- app/vysledky/[id]/page.tsx
+++ app/vysledky/[id]/page.tsx
@@ -18,18 +18,40 @@ export default async function ResultPage({ params }: Props) {
   const stats = getAssessmentStats(response.scores);
   return (
     <div className="max-w-[900px] m-auto flex flex-col gap-10 my-20">
-      <p>
-        Response ID {response.meta.id}, organization name{" "}
-        {response.meta.organizationName}.
-      </p>
-      <div className="flex flex-col gap-4">
-        <h2 className="text-2xl">Score Distribution</h2>
-        <BarChart
-          series={[{ data: Object.values(stats.scoreCountByScore) }]}
-          xAxis={[{ data: Object.keys(stats.scoreCountByScore) }]}
-          height={300}
-        />
-      </div>
+      <h1 className="text-3xl">
+        Scan results for “{response.meta.organizationName}”
+      </h1>
+      <ScoreDistributionSection distribution={stats.scoreCountByScore} />
     </div>
   );
 }
+
+const ScoreDistributionSection = ({
+  distribution,
+}: {
+  distribution: Record<string, number>;
+}) => {
+  const values = Object.entries(distribution);
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-2xl">Score Distribution</h2>
+      <BarChart
+        series={[{ data: values.map(second) }]}
+        xAxis={[
+          {
+            data: values.map(first),
+            colorMap: {
+              type: "ordinal",
+              values: ["0"],
+              colors: ["lightGray"],
+            },
+          },
+        ]}
+        height={300}
+      />
+    </div>
+  );
+};
+
+const first = <A, B>(pair: [A, B]) => pair[0];
+const second = <A, B>(pair: [A, B]) => pair[1];

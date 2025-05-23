5faee9860af64e7059e1774f68b87eca11b31372
Tomáš Znamenáček <zoul@zoul.cz>
2025-05-23 11:07:29 +0200

    Redirect / to production form

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "https://forms.fillout.com/t/ew3SxJA4UDus",
+      statusCode: 302,
+    },
+  ],
 };
 
 export default nextConfig;

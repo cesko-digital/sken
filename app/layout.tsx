73ebdcc1a06e7db132c6e456b75567820bb0abb9
Tomáš Znamenáček <zoul@zoul.cz>
2025-05-30 14:53:38 +0200

    Vendor & preload custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const customFont = localFont({
+  variable: "--font-cd",
+  src: [
+    { path: "../public/regular.woff2", weight: "400", style: "normal" },
+    { path: "../public/semibold.woff2", weight: "600", style: "semibold" },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Sken digitální vyspělosti",
@@ -12,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="cs">
       <head>
         <title>Sken digitální vyspělosti</title>
         <script
@@ -22,7 +31,7 @@ export default function RootLayout({
         ></script>
       </head>
       <body>
-        <main>{children}</main>
+        <main className={customFont.variable}>{children}</main>
         <footer>
           <p>
             Sken digitální vyspělosti provozuje{" "}

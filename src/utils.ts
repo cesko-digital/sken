fb24aea94e84327b296c19bce0bf5c5d028fd4bf
Tomáš Znamenáček <zoul@zoul.cz>
2025-07-02 10:39:45 +0200

    Add home page with lead capture form link

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,6 @@
 /** The URL of the Fillout form */
 export const formUrl = "https://forms.fillout.com/t/rMoYry5Shaus";
+
+/** The URL to lead capture form */
+export const leadFormUrl =
+  "https://crm.cesko.digital?entryPoint=leadCaptureForm&id=6862594be878a533b";

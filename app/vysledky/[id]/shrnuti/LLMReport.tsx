d09dd52c6453259c7eae9eef22d1941f66427038
Tomáš Znamenáček <zoul@zoul.cz>
2025-09-08 11:15:45 +0200

    Add skeleton loader for LLM report

--- app/vysledky/[id]/shrnuti/LLMReport.tsx
+++ app/vysledky/[id]/shrnuti/LLMReport.tsx
@@ -12,10 +12,20 @@ export const LLMReport = ({
   const { model, load } = useLLMReport(individualResponseId);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(load, [individualResponseId]);
+
   return (
     <div>
       {(model.state === "loading" || model.state === "idle") && (
-        <p>Generuji report…</p>
+        <section className="flex flex-col gap-8">
+          <Explainer />
+          <SkeletonLoader />
+        </section>
+      )}
+      {model.state === "loaded" && (
+        <section>
+          <Explainer />
+          <StyledMarkdown source={model.report} />
+        </section>
       )}
       {model.state === "failed" && (
         <p className="max-w-prose">
@@ -27,22 +37,33 @@ export const LLMReport = ({
           , opravíme. Pardon za potíže!
         </p>
       )}
-      {model.state === "loaded" && (
-        <section>
-          <p>
-            <mark>
-              Následující shrnutí bylo z vašich výsledků vygenerováno
-              automaticky pomocí jazykového modelu (LLM).
-            </mark>{" "}
-            Jako obvykle platí, že umělá inteligence je dobrý sluha, ale zlý
-            pán. Automatické shrnutí vám může pomoct nasměrovat pozornost na
-            správná místa, ale nespoléhejte na něj slepě, stroj se může snadno
-            splést. Rozumné je ověřit jeho tipy na číselných výsledcích nebo
-            konzultovat s expertem či expertkou. A teď už stroj:
-          </p>
-          <StyledMarkdown source={model.report} />
-        </section>
-      )}
+    </div>
+  );
+};
+
+const Explainer = () => (
+  <p>
+    <mark>
+      Následující shrnutí bylo z vašich výsledků vygenerováno automaticky pomocí
+      jazykového modelu (LLM).
+    </mark>{" "}
+    Jako obvykle platí, že umělá inteligence je dobrý sluha, ale zlý pán.
+    Automatické shrnutí vám může pomoct nasměrovat pozornost na správná místa,
+    ale nespoléhejte na něj slepě, stroj se může snadno splést. Rozumné je
+    ověřit jeho tipy na číselných výsledcích nebo konzultovat s expertem či
+    expertkou. A teď už stroj:
+  </p>
+);
+
+const SkeletonLoader = () => {
+  return (
+    <div role="status" className="max-w-sm animate-pulse flex flex-col gap-4">
+      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px]"></div>
+      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px]"></div>
+      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+      <span className="sr-only">Generuji report…</span>
     </div>
   );
 };

81e22295d257ac9effc9e93541f169a1333bb5f3
Tomáš Znamenáček <zoul@zoul.cz>
2025-09-09 10:01:02 +0200

    Redirect /vysledky/:id/shrnuti to /vysledky/:id

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://assets.cesko.digital/**")],
   },
+  redirects: async () => [
+    {
+      source: "/vysledky/:id/shrnuti",
+      destination: "/vysledky/:id",
+      permanent: true,
+    },
+  ],
 };
 
 export default nextConfig;
